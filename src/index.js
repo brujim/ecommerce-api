d190c7d38a68008228cb7b637aaa561a81b0f4d5
Bruno J Postali <103198140+bruno-postali-playkids@users.noreply.github.com>
2022-11-15 17:48:46 -0300

    added connections

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('Ola mundo')
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+require('dotenv').config({path: '.env'})
+
+const routes = require('./routes')
+
+const app = express()
+
+mongoose.connect(process.env.MONGO_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+}, console.log('Connected to database'))
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+app.listen(3000, () => console.log('Server is running!'))
